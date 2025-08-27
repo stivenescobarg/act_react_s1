3a31ee7e86a6d5ac2a2bb5b61c966cddcf04f5a8
stivenescobarg <stivenescobar240208@gmail.com>
2025-08-27 15:03:04 -0500

    Cambios

---
+++ components/Usuario.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+function Usuario(props) {
+  return (
+    <div className="bg-gray-100 p-4 m-2 rounded-lg">
+      <h2 className="text-xl font-bold">Nombre: {props.nombre}</h2>
+      <p>Edad: {props.edad} años</p>
+      <p>Ciudad: {props.ciudad}</p>
+    </div>
+  );
+}
+
+export default Usuario;
